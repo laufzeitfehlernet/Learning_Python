f6cb0b4635467bf17d8c0230cff8b761cdd43220
laufzeitfehlernet <32088292+laufzeitfehlernet@users.noreply.github.com>
2025-03-04 10:37:10 +0100

    Delete random directory
    obsolete

--- random/random02.py
+++
@@ -1,9 +0,0 @@
-### Create a random number just because of the microseconds 
-### of the actual time of the system
-
-import time
-
-myTime = time.time()
-
-microseconds = int((myTime - int(myTime)) * 10000000000000000)
-print(microseconds)
